ea135fd03a64b92bfe0a201147d6b0460c818c2a
SEC-Tito <mateoenclada52@gmail.com>
2024-05-10 11:09:39 -0500

    coño cebolla

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  currentTime: string = ""; // Inicializa la propiedad currentTime
+  imagenParaMostrar: string = "";
+  imagenTomada: any;
 
-  constructor() {}
+  constructor() {
+    this.updateTime(); // Inicia el reloj
+    setInterval(() => {
+      this.updateTime(); // Actualiza el reloj cada segundo
+    }, 1000);
+  }
 
+  ngOnInit(): void {
+    Camera.requestPermissions(); // Solicita permisos de la cámara cuando la página se inicializa
+  }
+
+  async getPicture() {
+    this.imagenTomada = await Camera.getPhoto({
+      quality: 90,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+
+    });
+    if (this.imagenTomada) {
+      this.imagenParaMostrar = this.imagenTomada.webPath;
+    }
+  }
+
+  updateTime() {
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes: any = now.getMinutes();
+    let seconds: any = now.getSeconds();
+
+    // Agrega ceros a la izquierda si es necesario para mantener el formato hh:mm:ss
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+    this.currentTime = hours + ':' + minutes + ':' + seconds;
+  }
 }
